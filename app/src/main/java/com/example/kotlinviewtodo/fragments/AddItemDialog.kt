51f9f31f867ed740c57c5367a7b3908f99464bed
Raymond Zeng <rzeng3@gmail.com>
2022-04-30 13:41:32 -0700

    Moved dialog to be its own layout

--- app/src/main/java/com/example/kotlinviewtodo/fragments/AddItemDialog.kt
+++ app/src/main/java/com/example/kotlinviewtodo/fragments/AddItemDialog.kt
@@ -1,4 +1,51 @@
 package com.example.kotlinviewtodo.fragments
 
-class AddItemDialog {
+import android.app.AlertDialog
+import android.app.Dialog
+import android.content.Context
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.DialogFragment
+import com.example.kotlinviewtodo.databinding.FragmentAddItemDialogBinding
+
+class AddItemDialog : DialogFragment() {
+    interface DialogResultListener { //Communicating b/t Activity and Fragment using an interface.
+        fun fireResult(string: String)
+    }
+
+    lateinit var result : DialogResultListener
+
+    override fun onAttach(context: Context) {
+        super.onAttach(context)
+        result = activity as DialogResultListener //Cast the activity as implemented interface
+    }
+
+    private var _binding : FragmentAddItemDialogBinding? = null
+    private val binding get() = _binding!!
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        _binding = FragmentAddItemDialogBinding.inflate(inflater, container, false)
+
+        binding.cancelButton.setOnClickListener {
+            dialog?.cancel()
+        }
+
+        binding.addButton.setOnClickListener {
+            result.fireResult(binding.newTask.text.toString())
+            dialog?.cancel()
+        }
+
+        return binding.root
+    }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
+    }
 }
