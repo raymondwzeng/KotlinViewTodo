d0fee8f66d207b59fc06e762d412036eb2869a26
Raymond Zeng <rzeng3@gmail.com>
2022-04-30 12:44:08 -0700

    Modified ListFragment to have the Activity as VM owner

--- app/src/main/java/com/example/kotlinviewtodo/fragments/ListFragment.kt
+++ app/src/main/java/com/example/kotlinviewtodo/fragments/ListFragment.kt
@@ -20,8 +20,8 @@ class ListFragment : Fragment() {
     ): View? {
         // Inflate the layout for this fragment.
         _binding = FragmentListBinding.inflate(inflater, container, false)
-        //Tie the viewmodel to the fragment, though we may end up actually wanting the owner to be the activity.
-        val model = ViewModelProvider(this)[(ListViewModel::class.java)]
+        //Tie the viewmodel to the fragment
+        val model = ViewModelProvider(requireActivity())[(ListViewModel::class.java)]
         val view = binding.root
         binding.recyclerView.adapter = ListAdapter(model.List)
         binding.recyclerView.layoutManager = LinearLayoutManager(this.context)
