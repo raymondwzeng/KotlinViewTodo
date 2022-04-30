74457cff2ae712cc720b21fc996249eef2e32fc2
Raymond Zeng <rzeng3@gmail.com>
2022-04-30 12:49:06 -0700

    Modified MainActivity to have data binding

--- app/src/main/java/com/example/kotlinviewtodo/MainActivity.kt
+++ app/src/main/java/com/example/kotlinviewtodo/MainActivity.kt
@@ -2,10 +2,15 @@ package com.example.kotlinviewtodo
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.kotlinviewtodo.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var _binding : ActivityMainBinding //Note: It seems like according to Google, only Fragments live after View is nuked. So no need to null this out?
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        _binding = ActivityMainBinding.inflate(layoutInflater)
+        val view = _binding.root
+        setContentView(view)
     }
 }
